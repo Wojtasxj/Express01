432ee950d0ba59c2fd4d8b13f9f5a58cac4d9a35
Wojciech Klause <wojtasxj@gmail.com>
2024-06-06 19:54:31 +0200

    20.4 stylowanie

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -30,6 +32,34 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.render('history');
+});
+
+app.post('/contact/send-message', upload.single('file'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+  const file = req.file;
+
+  // console.log('Author:', author);
+  // console.log('Sender:', sender);
+  // console.log('Title:', title);
+  // console.log('Message:', message);
+  // console.log('File:', file);
+
+  if (!author || !sender || !title || !message || !file) {
+    return res.render('contact', { isError: true, errorMessage: 'All fields, including file, are required.' });
+  }
+
+  const allowedExtensions = ['.png', '.jpg', '.jpeg', '.gif'];
+  const extension = path.extname(file.originalname).toLowerCase();
+  if (!allowedExtensions.includes(extension)) {
+    return res.render('contact', { isError: true, errorMessage: 'Invalid file type.' });
+  }
+
+  const successMessage = `The message has been sent! File ${file.originalname} has been saved.`;
+
+  res.render('contact', {
+    isError: false,
+    successMessage: successMessage
+  });
 });
 
 app.use((req, res) => {
